fbfe7fd01fd80362d2ec4eedea5914bbb6ccd618
gszo123 <gszo123@gmail.com>
2016-06-07 21:56:14 +0200

    Coverity analysis.

--- appveyor-build.cmd
+++ appveyor-build.cmd
@@ -1,11 +1,8 @@
 @echo off
 
-cd scintilla\boostregex                                                                 || exit
-buildboost.bat C:\projects\notepad-plus-plus\boost_1_55_0                               || exit
-cd ..\win32                                                                             || exit
-nmake -f scintilla.mak                                                                  || exit
-cd ..\..\PowerEditor\visual.net                                                         || exit
-msbuild notepadPlus.vcxproj /p:configuration="Unicode Debug" /p:platform=Win32          || exit
+cd PowerEditor\visual.net                                                               || exit
+cov-build --dir cov-int msbuild notepadPlus.vcxproj /p:configuration="Unicode Debug" /p:platform=Win32          || exit
 
-cd ..
-appveyor PushArtifact bin
+7z a cov.zip cov-int\*  || exit
+dir || exit
+
